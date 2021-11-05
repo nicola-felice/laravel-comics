3b9b18b9b9c6dda264799cd4d0571825f99b9bfb
nicolafelice <felicenicola99@hotmail.com>
2021-11-05 17:19:02 +0100

    add: footer

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -14,6 +14,6 @@
     @yield('mainContent')
   </main>
 
-  {{-- footer --}}
+  @include('partials.footer')
 </body>
 </html>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,73 @@
+<footer>
+  <div class="footer_top">
+    <div class="container">
+      <div>
+        <nav class="footer_nav">
+          <ul>
+            <h3>DC COMICS</h3>
+            <li><a href="#">characters</a></li>
+            <li><a href="#">comics</a></li>
+            <li><a href="#">movies</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">games</a></li>
+            <li><a href="#">videos</a></li>
+          </ul>
+        </nav>
+        <nav class="footer_nav">
+          <ul>
+            <h3>SHOP</h3>
+            <li><a href="#">shop DC</a></li>
+            <li><a href="#">shop DC collectibles</a></li>
+          </ul>
+        </nav>
+      </div>
+
+      <nav class="footer_nav">
+        <ul>
+          <h3>DC</h3>
+          <li><a href="#">terms of use</a></li>
+          <li><a href="#">privacy policy</a></li>
+          <li><a href="#">ad choices</a></li>
+          <li><a href="#">advertising</a></li>
+          <li><a href="#">jobs</a></li>
+          <li><a href="#">subscriptions</a></li>
+          <li><a href="#">talent workshops</a></li>
+          <li><a href="#">certificates</a></li>
+          <li><a href="#">ratings</a></li>
+          <li><a href="#">shop help</a></li>
+          <li><a href="#">contact us</a></li>
+        </ul>
+      </nav>
+
+      <nav class="footer_nav">
+        <ul>
+          <h3>SITES</h3>
+          <li><a href="#">DC</a></li>
+          <li><a href="#">MAD magazine</a></li>
+          <li><a href="#">DC univerce</a></li>
+          <li><a href="#">DC kids</a></li>
+          <li><a href="#">DC power visa</a></li>
+        </ul>
+      </nav>      
+
+      <img class="logo_footer_bg" src="{{asset("img/dc-logo-bg.png")}}" alt="">
+    </div>
+  </div>
+
+  <div class="footer_bottom">
+    <div class="container">
+      <div class="btn_wrapper">
+        <button class="sign_up_btn">SIGN-UP NOW!</button>
+      </div>
+
+      <div class="socials_wrapper">
+        <a class="follow_us" href="#">FOLLOW US</a>
+        <a href="#"><img src="{{asset("img/footer-facebook.png")}}" alt="icon social"></a>
+        <a href="#"><img src="{{asset("img/footer-twitter.png")}}" alt="icon social"></a>
+        <a href="#"><img src="{{asset("img/footer-youtube.png")}}" alt="icon social"></a>
+        <a href="#"><img src="{{asset("img/footer-pinterest.png")}}" alt="icon social"></a>
+        <a href="#"><img src="{{asset("img/footer-periscope.png")}}" alt="icon social"></a>
+      </div>
+    </div>
+  </div>
+</footer>
