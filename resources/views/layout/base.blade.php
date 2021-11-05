d374bb6a2f1f9a2be343ec31466c9871747114d5
nicolafelice <felicenicola99@hotmail.com>
2021-11-05 16:07:19 +0100

    add: header

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -8,7 +8,7 @@
   <link rel="stylesheet" href="{{asset("css/app.css")}}">
 </head>
 <body>
-  {{-- header --}}
+  @include('partials.header')
 
   <main>
     @yield('mainContent')
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,22 @@
+<header>
+  <div class="container">
+    <div class="logo_wrapper">
+      <a href="#"><img class="logo" src="{{asset("img/dc-logo.png")}}" alt="DC logo"></a>
+    </div>
+
+    <nav class="header_nav">
+      <ul>
+        <li><a href="#">characters</a></li>
+        <li><a class="active" href="#">comics</a></li>
+        <li><a href="#">movies</a></li>
+        <li><a href="#">TV</a></li>
+        <li><a href="#">games</a></li>
+        <li><a href="#">collectibles</a></li>
+        <li><a href="#">videos</a></li>
+        <li><a href="#">fans</a></li>
+        <li><a href="#">news</a></li>
+        <li><a href="#">shop</a></li>
+      </ul>
+    </nav>      
+  </div>
+</header>
