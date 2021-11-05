8870ed4ea733d7081e41a427abaee33533d52fa1
nicolafelice <felicenicola99@hotmail.com>
2021-11-05 15:51:56 +0100

    add: comics sec. + imgs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,26 @@
 @extends('layout.base')
 
 @section('mainContent')
-  <section>
-    <h1>prova</h1>
+
+  <section id="current_series_comics">
+    <div class="container">
+      <div class="label_current_series">current series</div>
+
+      <ul class="comics_list">
+        @foreach ($comics as $elm)
+          <li class="product">
+            <a href="#">
+              <div class="image_wrapper">
+                <img src="{{$elm["thumb"]}}" alt="{{$elm["type"]}}">
+              </div>
+              <span>{{$elm["series"]}}</span>
+            </a>
+          </li>            
+        @endforeach
+      </ul>
+
+      <button class="load_more_btn">load more</button>
+    </div>
   </section>
+
 @endsection
